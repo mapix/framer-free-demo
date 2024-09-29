856ab4ecedac5a7f1d43b7d5f53dc302c70f0b7a
mapix <mapix.me@gmail.com>
2024-09-29 16:23:45 +0800

    update more

---
+++ replace.sh
@@ -0,0 +1,8 @@
+
+https://unlimited-tribute-246920.framer.app/
+->
+http://0x2537.duckdns.org/
+
+./Atomic – Creative Business Landind Page Template for Framer_files/
+->
+https://framerusercontent.com/sites/6NQyFVDnk96jk7vseLBcl5/
